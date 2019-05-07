270dc9a11664f1ae99e231b903f23a681d671425
ChazComp <chaz@rag.net>
2019-05-07 08:31:47 -0600

    5/7/2019

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function set_H_Red()
 {
     for (let i = 0; i < document.getElementsByTagName('h2').length; i++)
     {
-        document.getElementsByTagName('h2')[i].style.background = "red";
+        document.getElementsByTagName('h2')[i].style.background = "#91121b";
     }
 }
 
